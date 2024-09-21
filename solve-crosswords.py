f2648262e800b954311576c1f1d9bb5b14823279
Luigi Foscari <luigi.foscari@icloud.com>
2024-09-21 14:41:11 +0200

    solver now does not make copies of the grid

--- solve-crosswords.py
+++ solve-crosswords.py
@@ -7,6 +7,7 @@ from copy import deepcopy
 
 H, V = "→", "↓"
 W, B = "_", "█"
+
 GRID = np.array([
     [B, W, B, W, B],
     [W, W, W, W, B],
@@ -16,35 +17,62 @@ GRID = np.array([
     [B, W, W, W, W],
 ])
 
-WORDS = [
-    [
-        "aa", "ab", "ba", "bb"
-    ], [
-        "aaa", "aab", "aba", "abb", "baa", "bab", "bba", "bbb"
-    ], [
-        "aaaa", "aaab", "aaba", "aabb", "abaa", "abab", "abba", "abbb",
-        "baaa", "baab", "baba", "babb", "bbaa", "bbab", "bbba", "bbbb"
-    ]
-]
-
-def get_word_candidates(length: int, intersecting = [], similar = []):
-    return WORDS[length - 2]
 
 def print_grid(grid):
     for line in grid: print(" ".join(line))
 
+
+# ---- WORDS ----
+
+WORDS = [
+    [
+        "aa", "ab", "ba", "bb"
+    ], [
+        "aab", "aba", "abb", "baa", "bab", "bba", "bbb"
+    ], [
+        "aaab", "aaba", "aabb", "abaa", "abab", "abba", "abbb",
+        "baaa", "baab", "baba", "babb", "bbaa", "bbab", "bbba", "bbbb"
+    ]
+]
+
+
+def get_word_candidates(bow, length: int, intersecting = [], similar = []):
+    return bow[length - 2]
+
+def exclude_word(bow, word):
+    bow[len(word) - 2].remove(word)
+
+
+# ---- WORD INSERTION AND DELETION ----
+
+def remove_word(grid, insertions):
+    for x, y in insertions: grid[x][y] = W
+
+
 def insert_word(grid, hook, word):
-    (direction, x, y, lenght) = hook
+    # Try to insert the word on the grid at the hook, if not possible clean up.
+    # Return True if managed to insert the word, signaling where changes were made.
+    
+    (direction, x, y, length) = hook
+    if length != len(word): return False
 
-    if lenght != len(word):
-        raise ValueError(f"Mismatch between word lenght and slot size: {len(word)} (word) != {lenght} (slot)!")
+    insertions = []
 
-    if direction == V:
-        grid = grid.T
-        x, y = y, x
+    for l in range(length):
+        i, j = (x, y + l) if direction == H else (x + l, y)
+        
+        if grid[i][j] == W:
+            grid[i][j] = word[l]
+            insertions.append((i, j))
 
-    grid[x][y:y+lenght] = list(word)
-    return grid.T if direction == V else grid
+        elif grid[i][j] != word[l]:
+            remove_word(grid, insertions)
+            return False, []
+
+    return True, insertions
+
+
+# ---- HOOKS ----
 
 def find_line_hooks(line):
     # Insert black square at the beginning and end of the line
@@ -57,12 +85,16 @@ def find_line_hooks(line):
     # Return these gaps and their respective lenghts
     return zip(blacks[minimum_lenght[0]], interval_lenghts[minimum_lenght] - 1)
 
+
 def find_hooks(grid):
     horizontal = [(row, find_line_hooks(line)) for row, line in enumerate(grid)]
     vertical = [(col, find_line_hooks(line)) for col, line in enumerate(grid.T)]
 
     return [(H, row, col, len) for row, value in horizontal for (col, len) in value] + \
         [(V, row, col, len) for col, value in vertical for (row, len) in value]
+
+
+# ---- SOLVER ----
 
 def solve_crossword(hooks, grid, bow):
     # todo: order hooks by length (longest to shortest)
@@ -73,31 +105,35 @@ def solve_crossword(hooks, grid, bow):
     for index, hook in enumerate(hooks):
         # find words of the right length for hook from bow
         # optionally filter for letters in the intersecting words
-        words = get_word_candidates(-1)
+
+        (_, _, _, length) = hook
+        words = get_word_candidates(bow, length)
 
         for word in words:
-            grid_copy = np.copy(grid)
-            grid_copy = insert_word(grid_copy, hook, word)
+            fits, insertions = insert_word(grid, hook, word)
+            if not fits: continue
+            
+            exclude_word(bow, word)
 
             remaining_hooks = hooks[index + 1:]
-            if solve_crossword(remaining_hooks, grid_copy, bow):
+            if solve_crossword(remaining_hooks, grid, bow):
                 return True
-            # remove word from hook (or simply don't do anything and grid_copy will be destroyed)
 
-    return False
+            remove_word(grid, insertions)
+
+        return False
 
 
 def main():
     grid = deepcopy(GRID)
-
     hooks = find_hooks(grid)
-    hook = hooks[-1]
-    grid = insert_word(grid, hook, "ciao")
 
-    print(hook)
-    print_grid(grid)
+    completed = solve_crossword(hooks, grid, WORDS)
+    if not completed:
+        print("Could not complete... sorry")
+    else:
+        print_grid(grid)
 
-    # solve_crossword(GRID, 0, 0)
 
 if __name__ == "__main__":
     main()
